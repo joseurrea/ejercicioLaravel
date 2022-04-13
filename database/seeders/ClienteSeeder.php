ff83162d50390fe87e2226fa0955774e7a2093ce
alumno <joseurrea1ro@gmail.com>
2022-04-13 14:07:13 +0200

    first commit

---
+++ database/seeders/ClienteSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ClienteSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\Cliente::factory(100)->create();
+    
+        //
+    }
+}
